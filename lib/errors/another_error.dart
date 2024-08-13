0f1713c26fd8d69ea7a742dbdb530829494a6e50
rekhan249 <rekhan2490@gmail.com>
2024-08-13 12:58:11 +0500

    file code extend with formatexception

---
+++ lib/errors/another_error.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+
+// Represent exceptions from Server/Remote data source.
+class RemoteException implements Exception {
+  DioException dioException;
+
+  RemoteException({required this.dioException});
+}
+
+// Represent exceptions from Cache.
+class LocalException implements Exception {
+  String error;
+  LocalException(this.error);
+}
+
+class RouteException implements Exception {
+  final String message;
+  RouteException(this.message);
+}
+class FormatException implements Exception {
+  final String message;
+  FormatException(this.message);
+}
